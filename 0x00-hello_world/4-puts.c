9cdb21f5a70f712ed8c9ec305a44fd37275de0eb
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-20 14:59:59 -0700

    a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void)
+/**
+ * This code contains the int main which is void
+ * and puts a string
+ * return is 0
+ */
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
