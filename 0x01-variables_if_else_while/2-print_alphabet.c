c40aa88f76ce3e7b7a33080b7968a88675940493
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-21 13:46:31 -0700

    script to print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - contains the code
+ * Description: script to print alphabets
+ * Return:0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
