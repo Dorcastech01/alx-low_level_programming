417a762abd4bfee64dbfc29b70c20167cb565095
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-21 15:09:21 -0700

    script for upper and lowercase alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,10 @@ int main(void)
 	char ch = 'a';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		putchar(ch);
-	}
-	putchar(toupper(ch));
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(toupper(ch));
 	putchar('\n');
 	return (0);
 }
