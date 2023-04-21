517c2ea4363e53328355a998600d2c7a8bf9890d
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-21 13:00:07 -0700

    script to get last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,20 +9,22 @@
 int main(void)
 {
 	int n;
+	int p;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ((n % 10) > 5)
+	p = n % 10;
+	if (p > 5)
 	{
-		printf("Last digit of %d is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, p);
 	}
-	else if ((n % 10) == 0)
+	else if (p == 0)
 	{
-		printf("Last digit of %d is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, p);
 	}
-	else if ((n % 10) < 0)
+	else if (p < 0)
 	{
-		printf("Last digit of %d is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
 	}
 	return (0);
 }
