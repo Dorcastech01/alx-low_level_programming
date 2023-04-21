a48aa2b9777d182e1cb9322ae9f780e66026bec5
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-20 18:08:50 -0700

    script using other outputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
+/**
  * main - contains void function
- * Description : a script with other output
- * Return : 1
+ * Description: a script with other output
+ * Return: 1 if the number is positive. 0 otherwise
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" -"
+" Dora Korpar, 2015-10-19\n");
 	return (1);
 }
