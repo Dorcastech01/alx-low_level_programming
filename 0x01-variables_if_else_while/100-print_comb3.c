32b7f76faf6e361744bb5e231d6ee260507b9ced
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-21 19:04:31 -0700

    script to combine two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: script to combine numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int d = 0;
+
+	for (c = 0; d < 10; d++)
+	{
+		for (c < 10; d < 10; c++)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+				if ((c + d) != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
