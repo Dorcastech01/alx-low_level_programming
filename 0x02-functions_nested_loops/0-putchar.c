df964dd672ad7490f94c297859f9fbd67ae1331b
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-25 08:18:14 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: a program to print a function
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	char *ch = "_putchar";
+
+	if (i == 0)
+		putchar(*ch);
+	putchar('\n');
+	return (0);
+}
