b9bedfe268407c6cb2fd909cc0702635d92af34e
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-25 16:10:33 -0700

    script to print absolute numbers only

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - main block
+ * @a: user input
+ * Description: script to print absolute numbers
+ * Return: 0
+ */
+int _abs(int a)
+{
+	int t;
+
+	if (a < 0)
+	{
+		t = a * -1;
+		_putchar(t);
+	}
+	else
+		_putchar(a);
+	return (0);
+}
