c0fe0f4e7d222c2bcbbbc35c9451febc1b2abdcc
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-25 18:35:02 -0700

    script to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - main block
+ * @e: user input 1
+ * @j: user input 2
+ * Description: script to add 2 integers
+ * Return: result
+ */
+int add(int e, int j)
+{
+	int result;
+
+	result = e + j;
+	return (result);
+}
