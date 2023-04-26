25a67935b7ac9dd55f6c6cb30393aa2526a54134
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-25 19:48:21 -0700

    script to sum multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main block
+ * Description: sum of multiples of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int result;
+
+	a = 0;
+	b = 0;
+	result = 0;
+
+	for (c = 0; c < 1024; c++)
+	{
+		if ((c % 3) == 0)
+		{
+			a = a + 1;
+		}
+
+		else if ((c % 5) == 0)
+		{
+			b = b + 1;
+		}
+	}
+	result = a + b;
+	printf("%d\n", result);
+	return (0);
+}
