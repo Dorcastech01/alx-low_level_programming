70d8a3685b786ff31554d7baa8e42682181d7739
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-25 18:26:05 -0700

    script for times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,19 +9,31 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int c;
 
-	a = 0;
-	while (a >= 48 && a <= 57)
+	for (a = 0; a < 10; a++)
 	{
-		b = 0;
-		while (b >= 48 && b <= 57)
+		for (b = 0; b < 10; b++)
 		{
-			_putchar(a * b);
-			_putchar(',');
-			_putchar(' ');
-			b++;
+			c = b * a;
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			if (c < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			} else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
 		}
 		_putchar('\n');
-		a++;
 	}
 }
