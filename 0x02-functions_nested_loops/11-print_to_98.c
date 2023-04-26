5d1e7a04c846a297897a74b802c7d287fdcae084
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-25 19:09:50 -0700

    script for range

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_to_98 - main block
+ * @n: user input
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+			{
+				_putchar(a);
+				_putchar('\n');
+			}
+			else if (a == 98)
+			{
+				_putchar(a);
+				_putchar('\n');
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			if (b != 98)
+			{
+				_putchar(b);
+				_putchar('\n');
+			}
+			else if (b == 98)
+			{
+				_putchar(b);
+				_putchar('\n');
+			}
+		}
+	}
+}
