64f058f287d4441e893e5ac64b2f54d9c9ef5886
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-27 16:29:40 -0700

    script to print fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,19 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0 && n % 3 != 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (n == 1)
 		{
@@ -29,8 +29,9 @@ int main(void)
 		}
 		else
 		{
-			printf(" %d", n);
+			printf("%d", n);
 		}
+		printf(" ");
 	}
 	putchar('\n');
 	return (0);
