6fbdf998d5aeba33d0c336b0d7be76cefc471c3b
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-27 06:29:56 -0700

    script to multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - main block
+ * @a: user first input
+ * @b: user second input
+ * Description: script to multiply two numbers
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
