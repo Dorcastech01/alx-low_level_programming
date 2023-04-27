0f16604ebcea93b7f784ac2ad770488ecc785216
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-27 07:50:18 -0700

    script to print numbers ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - main block
+ * Description: printing numbers 10x
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+	}
+	_putchar('\n');
+}
