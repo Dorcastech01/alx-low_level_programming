f2a556b0bdef30b444b7898a3dc1fc8dffa6a03b
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-27 16:15:32 -0700

    script to print fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Description: FizzBuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		printf("n ");
+		putchar('\n');
+	}
+	return (0);
+}
