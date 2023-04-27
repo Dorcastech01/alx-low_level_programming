4d9f4fff2191dab08411be60e799ed98bf2e167b
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-04-27 07:36:30 -0700

    print numbers but omit 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * print_most_numbers - main block
+ * Description: printing numbers but omitting 2 and 4
+ */
 void print_most_numbers(void)
 {
 	int y;
@@ -8,7 +11,9 @@ void print_most_numbers(void)
 	for (y = 0; y < 10; y++)
 	{
 		if (y != 2 && y != 4)
+		{
 			_putchar(y + '0');
+		}
 	}
 	_putchar('\n');
 }
