82d8bbae5f2b0f1e01cea8a494a6bec8459769c2
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-02 16:18:45 -0700

    script to print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - main block
+ * @str: user input
+ * Description: printing half of string
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int j, a;
+
+	j = 0;
+	while (str[j])
+	{
+		if (j % 2 == 0)
+		{
+			a = j % 2;
+			for (a = j % 2; a <= j; a++)
+			{
+				_putchar(str[a]);
+			}
+		}
+		else if (j % 2 != 0)
+		{
+			a = j % 2;
+			_putchar(str[a]);
+		}
+		j++;
+	}
+}
