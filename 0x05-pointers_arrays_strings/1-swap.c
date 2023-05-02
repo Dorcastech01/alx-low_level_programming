ebb38eedb73821dcb52da170ddf76de546b5dbfb
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-02 13:15:05 -0700

    script to swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - main block
+ * @a: first user input
+ * @b: second user input
+ * Description: swapping values of variables
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
