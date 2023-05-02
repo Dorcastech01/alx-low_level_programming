6b80c5397f5cefa02b29d17ef048e3c998861d6c
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-02 16:51:58 -0700

    script to print half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,28 +7,27 @@
  */
 void puts_half(char *str)
 {
-	int j, a;
+	int j, a, q;
 
 	j = 0;
-	while (str[j])
+	while (str[j] != 0)
 	{
 		j++;
-		if (j % 2 == 0)
+	}
+
+	if (j % 2 == 0)
+	{
+		for (a = j / 2; str[a] != 0; a++)
 		{
-			a = j / 2;
-			for (a = j / 2; a <= j; a++)
-			{
-				_putchar(str[a]);
-			}
-		}
-		else if (j % 2 != 0)
-		{
-			a = j % 2;
-			while (str[a])
-			{
-				_putchar(str[a]);
-			}
-			a++;
+			_putchar(str[a]);
 		}
 	}
+	else if (j % 2)
+	{
+		for (q = (j - 1) / 2; q < j - 1; q++)
+		{
+			_putchar(str[q + 1]);
+		}
+	}
+	_putchar('\n');
 }
