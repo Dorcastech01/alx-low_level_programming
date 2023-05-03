340c3f4fec79a4337e6fc1e95c97dfe4b8800999
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-02 17:08:03 -0700

    script to print number of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - main block
+ * @a: user input
+ * @n: number of array
+ * Description: printing number of array
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	n = 0;
+	while (a[n])
+	{
+		n++;
+	}
+
+	for (n = 0; a[n] != 0; n++)
+	{
+		putchar(a[n]);
+		printf(", ");
+	}
+	putchar('\n');
+}
