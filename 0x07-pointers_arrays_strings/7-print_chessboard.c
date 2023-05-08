2ba21ee51b69afb953f6af3435200d49e1deb70e
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-08 10:29:07 -0700

    function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - main function
+ * @a: variable
+ * Description: function that prints the chessboard
+ * Return: result
+ */
+void print_chessboard(char (*a)[8])
+{
+	int playerone, playertwo;
+
+	for (playerone = 0; playerone < 8; playerone++)
+	{
+		for (playertwo = 0; playertwo < 8; playertwo++)
+		{
+			_putchar(a[playerone][playertwo]);
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
