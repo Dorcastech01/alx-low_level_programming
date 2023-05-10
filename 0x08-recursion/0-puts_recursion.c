b2193c1aaaa68939aa29a8842698a8fc9b610361
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-10 08:06:18 -0700

    function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - main function
+ * Description: function that prints a string
+ * @s: arg
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_putchar('\n');
+}
