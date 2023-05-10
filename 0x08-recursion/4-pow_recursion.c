0e6b0e6e6a96d52a0bb359b28638d0cae4a9f7a7
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-10 09:34:07 -0700

    function for exponential

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - main function
+ * @x: first arg
+ * @y: second arg
+ * Description: exponential
+ * Return: result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (_pow_recursion(x, y));
+}
