07253254772bd4b517799f7c902d3b864d510fac
Dorcas Olanrewaju <dorcasolanrewaju_01@yahoo.com>
2023-05-24 08:46:35 -0700

    a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - main function
+ * @name: user's input
+ * @f: user's input
+ * Description: a function that prints a name
+ * Return: result
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
