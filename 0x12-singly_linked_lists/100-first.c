ba22a9e3d552b45cd191c7c5e9b9a04e5f562f1c
Dorcas Olanrewaju <dorcasolanrewaju_01@gmail.com>
2023-06-01 18:35:22 -0700

    a function that prints a string before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * all_words - main function
+ * Description: it prints a string before
+ * the main function is executed
+ * Return: void
+ */
+
+void all_words(void)
+{
+	char *h, *p;
+
+	h = "You're beat! and yet, you must allow,";
+	p = "I bore my house upon my back!";
+
+	printf("%s\n", h);
+	printf("%s\n", p);
+}
