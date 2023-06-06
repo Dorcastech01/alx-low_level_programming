8bead1c93b88c24196c49ba5592870b86523a32d
Dorcas Olanrewaju <dorcasolanrewaju_01@gmail.com>
2023-06-06 14:51:27 -0700

    a function that prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,26 +9,22 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *first, *second;
+	const listint_t *new_element;
 	size_t number;
 
-	first = head;
-	second = head;
+	new_element = head;
 	number = 0;
 
-	while (first != NULL && second != NULL && first->next != NULL)
+	while (new_element != NULL)
 	{
-		printf("[%p] %d\n", (void *)first, first->n);
+		printf("[%p] %d\n", (void *)new_element, new_element->n);
 		number++;
-		first = first->next;
+		new_element = new_element->next;
 
-		second = second->next->next;
-
-		if (first == second)
+		if (new_element >= new_element->next)
 		{
-			printf("[%p] %d\n", (void *)first, first->n);
-			number++;
-			break;
+			printf("[%p] %d\n", (void *)new_element, new_element->n);
+			exit(98);
 		}
 	}
 	return (number);
