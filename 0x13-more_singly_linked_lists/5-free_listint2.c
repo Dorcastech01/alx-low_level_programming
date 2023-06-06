96d4699ad2bafca02030a56e160cb328d476cae4
Dorcas Olanrewaju <dorcasolanrewaju_01@gmail.com>
2023-06-06 03:44:20 -0700

    a function that frees a linked list and sets head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,10 +14,11 @@ void free_listint2(listint_t **head)
 
 	if (*head == NULL)
 		return;
-	while (*head != NULL)
+	while (*head)
 	{
 		new_element = *head;
 		*head = (*head)->next;
 		free(new_element);
 	}
+	*head = NULL;
 }
