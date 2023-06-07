502877bbfb3ae2fe6ef30ecfa659142277cb2a42
Dorcas Olanrewaju <dorcasolanrewaju_01@gmail.com>
2023-06-06 17:56:18 -0700

    a function that prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,32 +1,91 @@
 #include "lists.h"
 
+size_t cc(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ * cc - this is a function
+ * @head: the head
+ * Description: a function that counts the number
+ * of unique elements in a linked list
+ * Return: result
+ */
+size_t cc(const listint_t *head)
+{
+	const listint_t *first, *second;
+	size_t third;
+
+	third = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	first = head->next;
+	second = (head->next)->next;
+
+	while (second)
+	{
+		if (first == second)
+		{
+			first = head;
+			while (first != second)
+			{
+				third++;
+				first = first->next;
+				second = second->next;
+			}
+
+			first = first->next;
+			while (first != second)
+			{
+				third++;
+				first = first->next;
+			}
+
+			return (third);
+		}
+
+		first = first->next;
+		second = (second->next)->next;
+	}
+
+	return (0);
+}
+
 /**
  * print_listint_safe - this is the main function
  * @head: the head
  * Description: a function that prints a linked list
- * Return: the number of nodes in the list
+ * Return: it will return the number of nodes in the list
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	long int first;
-	size_t second;
+	size_t third, number;
 
-	second = 0;
+	third = 0;
+	number = 0;
 
-	while (head)
+	third = cc(head);
+
+	if (third == 0)
 	{
-		first = head - head->next;
-		second++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (first > 0)
-			head = head->next;
-		else
+		for (; head != NULL; third++)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-			exit(98);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
-	return (second);
+
+	else
+	{
+		for (number = 0; number < third; number++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (third);
 }
