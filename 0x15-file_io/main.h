18b8e8312ff8b044c7319be4742f0c1291f1b1a6
Dorcas Olanrewaju <dorcasolanrewaju_01@gmail.com>
2023-06-13 17:19:06 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
